2889b7b92c0ce6b141beb783747ab2e28db109c5
Manish <mksjs1998@gmail.com>
2021-03-28 15:20:55 +0530

    paymentGtway-web initial commit

---
+++ src/main/java/com/paymentGtway/paymentGtwayweb/PaymentGtwayWebApplication.java
@@ -0,0 +1,13 @@
+package com.paymentGtway.paymentGtwayweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGtwayWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGtwayWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymentGtway/paymentGtwayweb/PaymentGtwayWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentGtway.paymentGtwayweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGtwayWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
